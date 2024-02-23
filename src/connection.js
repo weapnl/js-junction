74292b65b1bde83205c6b6f9a452e0a5966c96bc
Job Jenniskens <job@weap.nl>
2024-02-23 09:26:07 +0100

    Request cancel improvements

--- src/connection.js
+++ src/connection.js
@@ -11,8 +11,6 @@ export default class Connection {
         this.running = false;
         this.canceled = false;
         this.failed = false;
-
-        this._request = null;
     }
 
     cancel () {
@@ -23,8 +21,8 @@ export default class Connection {
         this.canceled = true;
     }
 
-    setRequest (request) {
-        this._request = request;
+    cancelRunning (request) {
+        this._api.cancelRunning(request);
     }
 
     setConfig (config) {
@@ -58,7 +56,7 @@ export default class Connection {
             url = `/${url}`;
         }
 
-        this._api.cancelRunning(this._request);
+        //this._api.cancelRunning(this._request);
 
         const config = {
             url: (this._api ? this._api.baseUrl : api.baseUrl) + url,
